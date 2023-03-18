15b20406d41a8bb5d09e89406542679c573c4d8e
aymen <107608230+alaymen11@users.noreply.github.com>
2023-03-18 10:11:02 +0300

    Banner alignment fixed

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -3,10 +3,9 @@ import React from "react";
 function Banner() {
   return (
     <div
-      className="w-full bg-cover bg-center h-[55vh] sm:h-[65vh] md:h-[93vh] xl:h-[98vh]"
+      className="w-full bg-cover bg-center h-[55vh] sm:h-[65vh] md:h-[93vh] xl:h-[98vh] overflow-hidden"
       style={{
-        backgroundImage:
-          "url(https://cdn-cfdja.nitrocdn.com/uOxxxDWjcukgNxoRlWCAPnOZsfFUWQlA/assets/images/optimized/rev-f9b6c16/wp-content/uploads/dedicated-team.jpg)",
+        backgroundImage: `url(/banner.png)`,
       }}
     >
       <div className="flex flex-col items-start justify-center h-full w-full bg-gradient-to-r from-[#4444447a] md:from-[#4444447a] to-[#4444447a] md:to-transparent bg-opacity-30 max-w-[1120px] ">
